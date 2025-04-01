11af6766dc8b68ee305bd31f1a4a4f42446c00a3
Soh-nia <soniatenajerah@icloud.com>
2025-04-01 20:49:44 +0100

    final commit

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    // page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+        <Table customers={customers} />
+      {/* <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div> */}
+    </div>
+  );
+}
